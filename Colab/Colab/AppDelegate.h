47f4531cafb8d7b5a79a9bfb1caf426945db51d5
Pinuno Fuentes <Calampunay@unknownf0b479211ec6.gateway.2wire.net>
2013-05-22 23:37:45 -0400

    ..

---
+++ Colab/Colab/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Colab
+//
+//  Created by Pinuno Fuentes on 5/22/13.
+//  Copyright (c) 2013 UNO IT Solutions, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Colab/Colab/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Colab
+//
+//  Created by Pinuno Fuentes on 5/22/13.
+//  Copyright (c) 2013 UNO IT Solutions, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
